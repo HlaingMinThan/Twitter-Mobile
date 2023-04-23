b9f953ad5f8de4dbc705ed36a3175596e47c4a6b
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-04-23 21:45:12 +0630

    character length warning color

--- screens/NewTweetScreen.jsx
+++ screens/NewTweetScreen.jsx
@@ -8,7 +8,6 @@ import profile from '../assets/react.jpg';
 export default function NewTweetScreen() {
     let tailwind = useTailwind();
     let [tweet, setTweet] = useState('');
-    let [error, setError] = useState('');
 
     let textMaxLength = 280;
 
@@ -16,14 +15,13 @@ export default function NewTweetScreen() {
         <View style={tailwind('bg-white h-full')}>
             <View style={tailwind('flex-row items-center justify-between p-3')}>
                 <View>
-                    <Text style={tailwind('text-gray-500')}>Characters left - {textMaxLength - tweet.length}</Text>
+                    <Text style={tailwind(`${tweet.length > 250 ? 'text-red-500' : 'text-gray-500 '}`)}>Characters left - {textMaxLength - tweet.length}</Text>
                 </View>
                 <View><Button tBgColor="bg-blue-500">Tweet</Button></View>
             </View>
             <View style={tailwind('flex-row')}>
                 <Avatar user={{ profile, }} size={3} style={tailwind('ml-3 mr-1')} />
                 <TextInput multiline maxLength={textMaxLength} onChangeText={setTweet} value={tweet} placeholder="What's Happening" style={tailwind('w-full h-full pb-10 flex-wrap pr-28')} ></TextInput>
-                {error && <Text style={tailwind('text-red-500 text-xs mt-1')}>{error}</Text>}
             </View>
         </View >
     )
