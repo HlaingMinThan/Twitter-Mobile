8f4d932829125ea3c71e3d5b59c31a6efc1a174f
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-04-23 12:30:46 +0630

    tab navigator and nested stacking

--- App.js
+++ App.js
@@ -1,41 +1,63 @@
-import { StyleSheet } from 'react-native';
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import TweetScreen from './screens/TweetScreen';
 import NewTweetScreen from './screens/NewTweetScreen';
 import SettingsScreen from './screens/SettingsScreen';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SearchScreen from './screens/SearchScreen';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
-const HomeStackNavigator = () => {
+const TabNavigator = () => {
+
   return (
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator screenOptions={{
+      headerShown:false
+    }}>
+      <Tab.Screen name="Home 1" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+    </Tab.Navigator>
+  )
+}
+
+const DrawerNavigator = () => {
+
+  return (
+    <Drawer.Navigator initialRouteName='Home' >
+      <Drawer.Screen name="Home" component={TabNavigator} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} />
+    </Drawer.Navigator>
+  )
+}
+
+const StackNavigator = () => {
+
+  return (
+      <Stack.Navigator screenOptions={({route}) => (
+        {
+          headerShown : route.name !== 'Home 2',
+          title : "",
+          headerBackTitleVisible : false
+        }
+      )}>
+        {/* show drawer first , if user click on apart of DrawerNavigator routes, it'll hide drawer */}
+        <Stack.Screen name="Home 2" component={DrawerNavigator} />
         <Stack.Screen name="Tweet" component={TweetScreen} />
         <Stack.Screen name="New Tweet" component={NewTweetScreen} />
       </Stack.Navigator>
   )
 }
+
 export default function App() {
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-          <Drawer.Screen name="Home" component={HomeStackNavigator} />
-          <Drawer.Screen name="Settings" component={SettingsScreen} />
-      </Drawer.Navigator>
+      <StackNavigator/>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
