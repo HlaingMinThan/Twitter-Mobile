eb8e29aa0b17afea05a0e2053bf27739d5494dfb
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-04-24 17:43:47 +0630

    add loading

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, Pressable } from 'react-native'
+import { View, Pressable, ActivityIndicator } from 'react-native'
 import { useTailwind } from 'tailwind-rn';
 import { AntDesign } from '@expo/vector-icons';
 import TweetsList from '../components/TweetsList';
@@ -9,10 +9,13 @@ import axios from 'axios';
 export default function HomeScreen({ navigation }) {
     const tailwind = useTailwind();
     let [tweets, setTweets] = useState([]);
+    let [isLoading, setIsLoading] = useState(false);
 
     let getTweets = async () => {
+        setIsLoading(true);
         const res = await axios.get('http://localhost:3000/tweets');
         setTweets(res.data)
+        setIsLoading(false);
     }
 
     useFocusEffect(
@@ -22,8 +25,9 @@ export default function HomeScreen({ navigation }) {
     );
 
     return (
-        <View style={tailwind('relative')}>
-            <TweetsList tweets={tweets} />
+        <View style={tailwind('relative h-full bg-white')}>
+            {isLoading && <ActivityIndicator size="large" color="gray" style={tailwind('mt-3')} />}
+            {!isLoading && <TweetsList tweets={tweets} />}
             <Pressable onPress={() => navigation.navigate('New Tweet')} style={tailwind('bg-blue-500 items-center justify-center w-12 h-12 rounded-full absolute bottom-6 right-5')}>
                 <AntDesign name="plus" size={20} color="white" />
             </Pressable>
