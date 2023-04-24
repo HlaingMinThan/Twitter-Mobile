6984f553f85e87cf63a0493a4636015d7a57b04b
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-04-24 17:39:24 +0630

    dynamic home page

--- components/TweetsList.jsx
+++ components/TweetsList.jsx
@@ -5,58 +5,12 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { FlatList, Pressable, Image, View, Text } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 import { useNavigation } from '@react-navigation/native';
+import moment from 'moment';
 
-export default function TweetsList({ ListHeaderComponent }) {
+export default function TweetsList({ ListHeaderComponent, tweets }) {
     let navigation = useNavigation();
 
     const tailwind = useTailwind();
-    const tweets = [
-        {
-            id: 1,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-        {
-            id: 2,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-        {
-            id: 3,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-        {
-            id: 4,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-        {
-            id: 5,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-        {
-            id: 6,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-        {
-            id: 7,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-        {
-            id: 8,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-        {
-            id: 9,
-            title: "Hello world",
-            description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        },
-    ]
 
     const TweetItem = ({ item }) => (
         <View style={tailwind('border-b-2 border-gray-100 w-full bg-white')}>
@@ -69,8 +23,8 @@ export default function TweetsList({ ListHeaderComponent }) {
                 })} HitRect={3}>
                     <View style={tailwind('flex-row mb-1')}>
                         <Text style={tailwind('mr-2 font-bold')} numberOfLines={1}>{item.title}</Text>
-                        <Text style={tailwind('mr-2 text-gray-500')} numberOfLines={1}>@hlaingminthan</Text>
-                        <Text style={tailwind('text-gray-500')}>. 9m</Text>
+                        <Text style={tailwind('mr-2 text-gray-500')} numberOfLines={1}>@{item.author.username}</Text>
+                        <Text style={tailwind('text-gray-500')}>. {moment(item.createdAt).fromNow(true)}</Text>
                     </View>
                     <Text style={tailwind('text-gray-700')} numberOfLines={4}>{item.description}</Text>
                     <View style={tailwind('flex-row mt-2 items-center')}>
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Pressable } from 'react-native'
 import { useTailwind } from 'tailwind-rn';
 import { AntDesign } from '@expo/vector-icons';
 import TweetsList from '../components/TweetsList';
+import { useFocusEffect } from '@react-navigation/native';
+import axios from 'axios';
 
 export default function HomeScreen({ navigation }) {
     const tailwind = useTailwind();
+    let [tweets, setTweets] = useState([]);
+
+    let getTweets = async () => {
+        const res = await axios.get('http://localhost:3000/tweets');
+        setTweets(res.data)
+    }
+
+    useFocusEffect(
+        React.useCallback(() => {
+            getTweets()
+        }, [])
+    );
 
     return (
         <View style={tailwind('relative')}>
-            <TweetsList />
+            <TweetsList tweets={tweets} />
             <Pressable onPress={() => navigation.navigate('New Tweet')} style={tailwind('bg-blue-500 items-center justify-center w-12 h-12 rounded-full absolute bottom-6 right-5')}>
                 <AntDesign name="plus" size={20} color="white" />
             </Pressable>
