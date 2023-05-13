ba0424615a6562d049d9cec344f5e243fb6dc783
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-05-13 11:14:09 +0630

    login method called

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { View, Text, TextInput, Pressable, Button } from 'react-native'
 import { useTailwind } from 'tailwind-rn'
+import { AuthContext } from '../contexts/AuthProvider';
 
 export default function LoginScreen({ navigation }) {
     let tailwind = useTailwind();
     let [email, setEmail] = useState('');
     let [password, setPassword] = useState('');
+    let { login } = useContext(AuthContext);
 
     return (
         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
@@ -13,7 +15,7 @@ export default function LoginScreen({ navigation }) {
             <TextInput value={email} onChangeText={setEmail} style={tailwind('border w-80 my-3 border-gray-400 p-2')} placeholder='Email'></TextInput>
             <TextInput value={password} onChangeText={setPassword} style={tailwind('border w-80 my-3 border-gray-400 p-2')} placeholder='Password'></TextInput>
             <Pressable style={tailwind('bg-blue-500 px-6 py-2 rounded-full')}>
-                <Text style={tailwind('text-white text-lg')}>Log In</Text>
+                <Text style={tailwind('text-white text-lg')} onPress={login}>Log In</Text>
             </Pressable>
             <View style={tailwind('flex-row items-center justify-center mt-3')}>
                 <Text>Don't have an account ?</Text>
